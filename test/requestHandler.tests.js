c0cc4f4b2089884fe6c5b593ea28644d3ddcd9ef
Simon Crabtree <simon@simoncrabtree.net>
2012-10-09 17:18:55 +0100

    Server now handles /js requests - serving out Require.js and client loads an example AMD module (main.js)

--- test/requestHandler.tests.js
+++ test/requestHandler.tests.js
@@ -31,4 +31,34 @@ describe("The Request Handler", function () {
 
         handler(req, res);
     });
+
+    it("Handles requests for /js files", function () {
+        var res = {
+            end: function (body) {
+                should.exist(body);
+                body.toString().should.contain('RequireJS');
+            }
+        };
+
+        var req = {
+            url: '/js/require.js'
+        };
+
+        handler(req, res);
+    });
+
+    it("Returns a 404 if js file is not found", function (done) {
+        var res = {
+            end: function () {
+                this.statusCode.should.equal(404);
+                done();
+            }
+        };
+
+        var req = {
+            url: '/js/FileWhichDoesntExist'
+        };
+
+        handler(req, res);
+    });
 });
